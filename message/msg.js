8315fdc3cd78cd11d53ad5233200e7876e2da186
NaiyoQiqi <faifaizal933@gmail.com>
2025-01-28 19:12:37 +0700

    Update msg.js

--- message/msg.js
+++ message/msg.js
@@ -1,1 +1,14 @@
+const { callGeminiAI } = require('../index'); // Import fungsi callGeminiAI
+const moment = require('moment'); // Import moment untuk format waktu
+const util = require('util'); // Untuk menggunakan setTimeout versi Promise
+const setTimeoutPromise = util.promisify(setTimeout); // Membuat setTimeout versi Promise
 
+module.exports = async (conn, msg, m) => {
+    if (msg.message.conversation) {
+        const aiResponse = await callGeminiAI(msg.message.conversation);
+        const time = moment(new Date()).format('HH:mm:ss DD/MM/YYYY');
+        const aiResponseWithFlag = `${aiResponse} | ai: true`; // Tambahkan label "ai: true" di samping waktu pesan
+        await setTimeoutPromise(3000); // Tambahkan delay 3 detik
+        await conn.reply(msg.key.remoteJid, `[${time}] ${aiResponseWithFlag}`, msg);
+    }
+};
